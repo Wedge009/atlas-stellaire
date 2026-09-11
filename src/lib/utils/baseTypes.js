d3fd4a429129ee05f0bd9120816acc035b084d04
Wedge009 <wedge009@wedge009.net>
2026-09-12 00:13:31 +1000

    Remove Gaea and Oxford sprites.
    It does not appear they are actually unique, in-game.

--- src/lib/utils/baseTypes.js
+++ src/lib/utils/baseTypes.js
@@ -2,13 +2,13 @@ const base = import.meta.env.BASE_URL;
 
 const BASE_TYPE_ICONS = {
   agricultural: `${base}assets/bases/agricultural.png`,
-  gaea: `${base}assets/bases/gaea.png`,
+  gaea: `${base}assets/bases/agricultural.png`,
   mining: `${base}assets/bases/mining.png`,
   pirate: `${base}assets/bases/pirate.png`,
   steltek: `${base}assets/bases/steltek.png`,
   'new-constantinople': `${base}assets/bases/new-constantinople.png`,
   'new-detroit': `${base}assets/bases/new-detroit.png`,
-  oxford: `${base}assets/bases/oxford.png`,
+  oxford: `${base}assets/bases/pleasure.png`,
   perry: `${base}assets/bases/perry.png`,
   pleasure: `${base}assets/bases/pleasure.png`,
   refinery: `${base}assets/bases/refinery.png`,
